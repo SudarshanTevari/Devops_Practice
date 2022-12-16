a1f6fc7e58b737cbc5934b981d1060442d9b4bea
Sudarshan <sudarshantevari@gmail.com>
2022-12-16 18:48:07 +0000

    script update to test webhook

--- jenkins/multi_node_deployment.groovy
+++ jenkins/multi_node_deployment.groovy
@@ -4,21 +4,21 @@ pipeline{
         stage('Bilid'){
             steps{
                 node('node1'){
-                    sh 'echo "Build stage running on `hostname` server" '
+                    sh 'echo "Build stage running on `hostname` server!" '
                 }
             }
         }
         stage('Test'){
             steps{
                 node('node2'){
-                    sh 'echo "Test stage running on `hostname` server" '
+                    sh 'echo "Test stage running on `hostname` server!" '
                 }
             }
         }
         stage('Deploy'){
             steps{
                 node('node1'){
-                    sh 'echo "Deploy stage runnind on `hostname` server" '
+                    sh 'echo "Deploy stage runnind on `hostname` server!" '
                 }
             }
         }
