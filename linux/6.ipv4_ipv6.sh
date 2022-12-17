7077d32c87cd70d50c1134ecbbe7ded98a2b63d7
Sudarshan <sudarshantevari@gmail.com>
2022-12-17 20:17:44 +0000

    Script to get ip adderss details

--- linux/6.ipv4_ipv6.sh
+++ linux/6.ipv4_ipv6.sh
@@ -4,11 +4,12 @@
 echo "---------------------Private IPV4---------------------"
 hostname -I
 
-echo "-------------------------IPV6-------------------------"
-ip addr | grep inet6 | awk 'NR>1' | awk '{print $2}'
-
 echo "---------------------Public IPV4----------------------"
 curl ifconfig.me 
 echo
+
+echo "-------------------------IPV6-------------------------"
+ip addr | grep inet6 | awk 'NR>1' | awk '{print $2}'
+
 
 
