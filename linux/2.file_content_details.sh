8b6d3a353e8ef3a3717e049978be683c5e54c5f8
Sudarshan <sudarshantevari@gmail.com>
2022-12-20 06:59:11 +0000

    Updted Script

--- linux/2.file_content_details.sh
+++ linux/2.file_content_details.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-read -p "Enter file name to dispaly details: " file
+read -p "Enter file name to dispaly details: " file_name
 
-wc $file
+echo "Number of lines in file \"$file_name\" are: " `cat $file_name | wc -l`
+echo "Number of words in file \"$file_name\" are: " `cat $file_name | wc -w`
+echo "Number of characters in file \"$file_name\" are: " `cat $file_name | wc -m`
+echo "Size of file \"$file_name\" in byte: " `cat $file_name | wc -c`
+
+
