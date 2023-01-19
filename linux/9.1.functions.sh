f60ebdb2b9dfdaf1225745302d65a7e705a5174f
Sudarshan <sudarshantevari@gmail.com>
2023-01-19 17:47:53 +0530

    Script to call function from another scritp

---
+++ linux/9.1.functions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+user()
+{
+	echo "Sudarshan"
+}
+
+msg()
+{
+	echo "Have a good day"
+}
---
+++ linux/9.2.fun_call.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+source /home/opc/test/9.1.functions.sh
+
+echo "This is called from antother file (user name):" `user`
+
+echo "This is called from antother file (message):" `msg`
